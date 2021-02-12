6ce0c5445c485737a26bcd692819e94d634d7ea4
jairsantos <jairsantos-jau@hotmail.com>
2021-02-12 18:35:10 -0300

    projeto reactback avancado

---
+++ pages/components/Button.js
@@ -0,0 +1,13 @@
+import styles from './Button.module.css'
+
+
+export default function Button() {
+    return (
+        <button type="button"
+        className={styles.error}
+        
+        >
+            Destroy
+        </button>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+import Formlario from "./components/Formulario/index";
+
+function HomePage(){
+    return (
+    <div>Bem vindos,Aqui voce pode desabilita o JavaScript
+    <Formlario/>
+    <Button/>
+
+    </div>
+    )
+}
+export default HomePage
